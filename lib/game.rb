a6424a165c0582e486af2143756ddcfbb359bf1b
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2022-01-28 11:14:27 -0600

    updated attr_reader and accessor for Player class

--- lib/game.rb
+++ lib/game.rb
@@ -1,29 +1,31 @@
-
-
-
 class Game
-
-  def initialize (player)
-    @player_name = player_name
+  def initialize
     @number_of_rounds = 12
     #@answer = CSV File.sample if word is between 5 and 12 characters long
     #@working_answer = @answer.length of "_"
   end
 
   def play
-    @player.guess_sequence
+    loop do
+      player.guess_sequence
 
-    if player.has_won?
-      #end game
-    elsif player.has_lost?
-      #end game
-    else
-      @number_of_rounds -= 1
+      if player.has_won?
+        puts "#{player.name} guessed the sequence! Great job!"
+        puts "#{answer}"
+        break
+      elsif player.has_lost?
+        puts 'Ahhhh you ran out of turns. Beter luck next time.'
+        break
+      else
+        @number_of_rounds -= 1
+      end
     end
   end
 end
 
 class Player
+  attr_reader :name
+  attr_accessor :guesses
 
   def initialize
     puts "Let's play some old fashioned Hangman. What is your name?"
@@ -44,3 +46,7 @@ class Player
     game.number_of_rounds == 0
   end
 end
+
+player = Player.new
+
+Game.new(player)
